3b2718bd99f9aeeaa9dcba2ce222a38f210783f1
nosepato <felipe.pinhoramos@outlook.com>
2024-06-05 23:09:31 -0300

    carrossel pronto

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,39 +1,81 @@
 import React from 'react';
 import { Carousel } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import camisadoreal from '../images/camisadoreal.jpg';
+import camisadoreal1 from '../images/camisadoreal1.jpg';
 import camisadoreal2 from '../images/camisadoreal2.jpg';
 import camisadoreal3 from '../images/camisadoreal3.jpg';
+import camisadocity1 from '../images/camisadocity1.jpg';
+import camisadocity2 from '../images/camisadocity2.jpg';
+import camisadocity3 from '../images/camisadocity3.jpg';
+import camisadomilan from '../images/camisadomilan.jpg';
+import camisadainter from '../images/camisadainter.jpg';
+import camisadajuve from '../images/camisadajuve.jpg';
 
 const App = () => {
   return (
     <div className="carousel-container">
       <Carousel
-        interval={null}
+        interval={2000} // Alterado para mudar automaticamente a cada 2 segundos (2000ms)
         indicators={false}
         controls={true}
         className="custom-carousel"
       >
         <Carousel.Item>
-          <img
-            className="d-block custom-carousel-img"
-            src={camisadoreal}
-            alt="First slide"
-          />
+          <div className="d-flex justify-content-between">
+            <img
+              className="custom-carousel-img"
+              src={camisadoreal1}
+              alt="First slide"
+            />
+            <img
+              className="custom-carousel-img"
+              src={camisadoreal2}
+              alt="Second slide"
+            />
+            <img
+              className="custom-carousel-img"
+              src={camisadoreal3}
+              alt="Third slide"
+            />
+          </div>
         </Carousel.Item>
         <Carousel.Item>
-          <img
-            className="d-block custom-carousel-img"
-            src={camisadoreal2}
-            alt="Second slide"
-          />
+          <div className="d-flex justify-content-between">
+            <img
+              className="custom-carousel-img"
+              src={camisadocity1}
+              alt="Fourth slide"
+            />
+            <img
+              className="custom-carousel-img"
+              src={camisadocity2}
+              alt="Fifth slide"
+            />
+            <img
+              className="custom-carousel-img"
+              src={camisadocity3}
+              alt="Sixth slide"
+            />
+          </div>
         </Carousel.Item>
         <Carousel.Item>
-          <img
-            className="d-block custom-carousel-img"
-            src={camisadoreal3}
-            alt="Third slide"
-          />
+          <div className="d-flex justify-content-between">
+            <img
+              className="custom-carousel-img"
+              src={camisadomilan}
+              alt="Fourth slide"
+            />
+            <img
+              className="custom-carousel-img"
+              src={camisadajuve}
+              alt="Fifth slide"
+            />
+            <img
+              className="custom-carousel-img"
+              src={camisadainter}
+              alt="Sixth slide"
+            />
+          </div>
         </Carousel.Item>
       </Carousel>
     </div>

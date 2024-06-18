97b89e72f2b0483894653c2862dd0d6e5b18785d
nosepato <felipe.pinhoramos@outlook.com>
2024-06-17 22:21:38 -0300

    atualizando a grade de produtos

--- src/components/ProductGrid.js
+++ src/components/ProductGrid.js
@@ -1,29 +1,27 @@
 import React, { useState } from 'react';
-import camisadoreal1 from '../images/camisadoreal1.jpg'
-import camisadoreal2 from '../images/camisadoreal2.jpg'
-import camisadocity1 from '../images/camisadocity1.jpg'
-import camisadocity2 from '../images/camisadocity2.jpg'
-import camisadainter from '../images/camisadainter.jpg'
-import camisadajuve from '../images/camisadajuve.jpg'
-import camisadomilan from '../images/camisadomilan.jpg'
-
+import camisadoreal1 from '../images/camisadoreal1.jpg';
+import camisadoreal2 from '../images/camisadoreal2.jpg';
+import camisadocity1 from '../images/camisadocity1.jpg';
+import camisadocity2 from '../images/camisadocity2.jpg';
+import camisadainter from '../images/camisadainter.jpg';
+import camisadajuve from '../images/camisadajuve.jpg';
+import camisadomilan from '../images/camisadomilan.jpg';
 
 const products = [
-  { id: 1, category: 'Espanha', name: 'Real Madrid 1', image: camisadoreal1 },
-  { id: 2, category: 'Espanha', name: 'Real Madrid 2', image: camisadoreal2 },
-  { id: 3, category: 'Inglaterra', name: 'Man City 1', image: camisadocity1 },
-  { id: 4, category: 'Inglaterra', name: 'Man City 2', image: camisadocity2 },
-  { id: 5, category: 'Itália', name: 'Inter de Milão', image: camisadainter },
-  { id: 6, category: 'Itália', name: 'Juventus', image: camisadajuve },
-  { id: 7, category: 'Itália', name: 'Milan', image: camisadomilan }
+  { id: 1, category: 'Espanha', name: 'Real Madrid 1', image: camisadoreal1, price: 'R$120,00' },
+  { id: 2, category: 'Espanha', name: 'Real Madrid 2', image: camisadoreal2, price: 'R$120,00' },
+  { id: 3, category: 'Inglaterra', name: 'Man City 1', image: camisadocity1, price: 'R$120,00' },
+  { id: 4, category: 'Inglaterra', name: 'Man City 2', image: camisadocity2, price: 'R$120,00' },
+  { id: 5, category: 'Itália', name: 'Inter de Milão', image: camisadainter, price: 'R$120,00' },
+  { id: 6, category: 'Itália', name: 'Juventus', image: camisadajuve, price: 'R$120,00' },
+  { id: 7, category: 'Itália', name: 'Milan', image: camisadomilan, price: 'R$120,00' }
 ];
 
-//filtro
 function ProductGrid() {
   const [category, setCategory] = useState('Todos');
 
   const filteredProducts = category === 'Todos' ? products : products.filter(product => product.category === category);
-  
+
   return (
     <div className="product-grid">
       <h2 className='produtos'>Produtos</h2>
@@ -40,6 +38,8 @@ function ProductGrid() {
           <div key={product.id} className="product">
             <img src={product.image} alt={product.name} className="product-image" />
             <div className="product-name">{product.name}</div>
+            <button className="product-button">Ver Mais</button>
+            <div className="product-price">{product.price}</div>
           </div>
         ))}
       </div>

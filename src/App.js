f69d835c2abfb6ecd62841d8ce1c709c5676b949
nosepato <felipe.pinhoramos@outlook.com>
2024-07-03 18:47:08 -0300

    final update 3.3

--- src/App.js
+++ src/App.js
@@ -16,19 +16,22 @@ import ProductInter from './components/ProductInter';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/product/real-madrid-1" element={<ProductPage Component={ProductReal1} />} />
-        <Route path="/product/real-madrid-2" element={<ProductPage Component={ProductReal2} />} />
-        <Route path="/product/man-city-1" element={<ProductPage Component={ProductCity1} />} />
-        <Route path="/product/man-city-2" element={<ProductPage Component={ProductCity2} />} />
-        <Route path="/product/juventus" element={<ProductPage Component={ProductJuve} />} />
-        <Route path="/product/milan" element={<ProductPage Component={ProductMilan} />} />
-        <Route path="/product/inter-milao" element={<ProductPage Component={ProductInter} />} />
-        <Route path="/cart" element={<CartPage />} />
-      </Routes>
-    </Router>
+    <div className="app-container">
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/product/real-madrid-1" element={<ProductPage Component={ProductReal1} />} />
+          <Route path="/product/real-madrid-2" element={<ProductPage Component={ProductReal2} />} />
+          <Route path="/product/man-city-1" element={<ProductPage Component={ProductCity1} />} />
+          <Route path="/product/man-city-2" element={<ProductPage Component={ProductCity2} />} />
+          <Route path="/product/juventus" element={<ProductPage Component={ProductJuve} />} />
+          <Route path="/product/milan" element={<ProductPage Component={ProductMilan} />} />
+          <Route path="/product/inter-milao" element={<ProductPage Component={ProductInter} />} />
+          <Route path="/cart" element={<CartPage />} />
+        </Routes>
+      </Router>
+      <Footer />
+    </div>
   );
 }
 
@@ -36,10 +39,11 @@ function HomePage() {
   return (
     <>
       <Header />
-      <Carousel />
-      <AdsSection />
-      <ProductGrid />
-      <Footer />
+      <main>
+        <Carousel />
+        <AdsSection />
+        <ProductGrid />
+      </main>
     </>
   );
 }
@@ -48,7 +52,9 @@ function ProductPage({ Component }) {
   return (
     <>
       <Header />
-      <Component />
+      <main>
+        <Component />
+      </main>
     </>
   );
 }
@@ -57,7 +63,9 @@ function CartPage() {
   return (
     <>
       <Header />
-      <Cart />
+      <main>
+        <Cart />
+      </main>
     </>
   );
 }
